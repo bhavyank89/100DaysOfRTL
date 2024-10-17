8fef25457499601c6aaefbe6fbb78548a4f91d83
bhavyank89 <bhavyanksarolia@gmail.com>
2024-10-17 09:39:50 +0530

    Day038

---
+++ Day038 - binary to grey convertor/design.sv
@@ -0,0 +1,11 @@
+// Code your design here
+module binary_to_grey_4Bit(G,B);
+  input [3:0] B;
+  output [3:0] G;
+  
+  assign G[3] = B[3];
+  assign G[2] = B[3]^B[2];
+  assign G[1] = B[2]^B[1];
+  assign G[0] = B[1]^B[0];
+  
+endmodule
